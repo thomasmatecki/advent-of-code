da269944138b15025c4b877c16ddb55b5331ae9e
Thomas Matecki <thomas.matecki@gmail.com>
2024-12-13 03:02:07 +0000

    day 12

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -2,6 +2,7 @@ from functools import cache
 from itertools import chain
 
 
+@cache
 def change(n):
     str_n = str(n)
     length = len(str_n)
@@ -18,7 +19,7 @@ def change(n):
 def breadth(stone, depth):
     stones = [stone]
     for _ in range(depth):
-        stones = chain.from_iterable(change(s) for s in stones)
+        stones = chain.from_iterable(map(change, stones))
     return tuple(stones)
 
 
@@ -26,22 +27,37 @@ def breadth(stone, depth):
 def depth(stone, i):
     l = 0
     for j in range(i, 0, -1):
-        adv = breadth(stone, depth=3)
+        adv = breadth(stone, depth=5)
         if (k := j-1) >= 1:
-            stone, *tail = adv
+            stone, tail = adv[0], adv[1:]
             l += iter_depth(tail, k)
 
-    l += len(adv)
+    l += sum(1 for _ in adv)
     return l
 
 
+@cache
 def iter_depth(stones, i):
     return sum(depth(stone, i) for stone in stones)
 
 
-if __name__ == "__main__":
-    with open("2024/day11/input.txt") as f:
-        stones = [*map(int, f.read().split())]
+def load(filename):
+    with open(f"2024/day11/{filename}") as f:
+        return tuple(map(int, f.read().split()))
 
-    result = iter_depth(stones, 25)
+
+def part_one():
+    stones = load("input.txt")
+    result = iter_depth(stones, 4)
     print(result)
+
+
+def part_two():
+    stones = load("input.txt")
+    result = iter_depth(stones, 15)
+    print(result)
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
---
+++ 2024/day12/day12.py
@@ -0,0 +1,111 @@
+
+
+from collections import UserList, deque
+from itertools import groupby
+from operator import itemgetter
+
+
+class Grid(UserList):
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(f"2024/day12/{filename}") as file:
+            return cls(map(str.strip, file.read().splitlines()))
+
+    def neighbors(self, i, j):
+        for i0, j0 in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            yield i0, j0
+
+    def in_bounds(self, i, j):
+        return 0 <= i < len(self) and 0 <= j < len(self[0])
+
+    def explore(self, i, j, visited):
+        v = self[i][j]
+        region = dict()
+        q = {(i, j)}
+
+        while q:
+            i, j = q.pop()
+            region[(i, j)] = 0
+            for i0, j0 in self.neighbors(i, j):
+                if not self.in_bounds(i0, j0):
+                    region[(i, j)] += 1
+                elif (i0, j0) in visited:
+                    region[(i, j)] += 1
+                elif (i0, j0) in region:
+                    continue
+                elif self[i0][j0] == v:
+                    q.add((i0, j0))
+                else:
+                    region[(i, j)] += 1
+
+        return region
+
+    def regions(self):
+
+        visited = set()
+        regions = []
+
+        for i, row in enumerate(self):
+            for j, _ in enumerate(row):
+                if (i, j) in visited:
+                    continue
+                region = self.explore(i, j, visited)
+                visited.update(region.keys())
+                regions.append(region)
+
+        return regions
+
+    def _disjoints(self, xs):
+        xs = sorted(xs)
+        x = [_x for _x, _ in xs]
+        return 1 + sum(1 for n in range(1, len(x))
+                       if x[n]-x[n-1] > 1)
+
+    def _collate_edges(self, edges, group, get_v):
+        return sum(
+            self._disjoints(map(get_v, vs))
+            for g, vs in groupby(sorted(edges, key=group), group)
+            if g[0] != g[1]
+        )
+
+    def count_sides(self, region):
+        edges = []
+        for i0, j0 in region:
+            edges.extend(
+                ((i0, i1), (j0, j1))
+                for i1, j1 in self.neighbors(i0, j0)
+                if (i1, j1) not in region
+            )
+
+        get_i = itemgetter(0)
+        get_j = itemgetter(1)
+
+        return (
+            self._collate_edges(edges, get_i, get_j)
+            + self._collate_edges(edges, get_j, get_i)
+        )
+
+
+def part_one(filename):
+    grid = Grid.from_file(filename)
+    total_price = sum(len(r) * sum(r.values()) for r in grid.regions())
+    print(f"part one ({filename}): {total_price}")
+
+
+def part_two(filename):
+
+    grid = Grid.from_file(filename)
+    total_price = sum(len(r) * grid.count_sides(r) for r in grid.regions())
+
+    print(f"part two ({filename}): {total_price}")
+
+
+if __name__ == "__main__":
+    part_one("test1.txt")
+    part_one("test2.txt")
+    part_one("input.txt")
+
+    part_two("test2.txt")
+    part_two("test3.txt")
+    part_two("input.txt")
